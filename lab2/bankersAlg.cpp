917ec2b9aa4e92db5ce76218a600af7d01061c77
Yug Patel <ypatel10@kent.edu>
2023-11-29 19:02:37 -0500

    Added main and txt file

---
+++ lab2/bankersAlg.cpp
@@ -0,0 +1,15 @@
+//
+// file: bankersAlg.cpp
+// author: Yug Patel
+// last modified: 29 November 2023
+//
+#include <iostream>
+#include <fstream>
+using std::cout; using std::cin; using std::endl;
+
+const int numProcesses = 5;
+const int numResources = 3;
+
+void resouceAllocation(){
+    
+}
